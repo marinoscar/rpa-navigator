8de91122da5791c8eaaf53d3459e9e8e3ff73970
Oscar Marin <oscar@marin.cr>
2018-09-02 17:09:39 -0600

    added choise and wait stages

---
+++ code/luval.rpa.common/Model/WaitStage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace luval.rpa.common.Model
+{
+    public class WaitStage: Stage
+    {
+        public WaitStage(XElement el): base(el)
+        {
+            LoadChoices();
+        }
+
+        private void LoadChoices()
+        {
+            Choices = new List<Choice>();
+            var choices = Xml.Elements().Where(i => i.Name.LocalName == "choices").ToList();
+            foreach(var choice in choices)
+            {
+                Choices.Add(new Choice(choice));
+            }
+        }
+
+        public List<Choice> Choices { get; set; }
+
+        public string Description
+        {
+            get { return GetElementValue("narrative"); }
+            set { TrySetElValue("narrative", value); }
+        }
+
+        public string Timeout
+        {
+            get { return GetElementValue("timeout"); }
+            set { TrySetElValue("timeout", value); }
+        }
+
+        public bool IsTimeoutHardCoded
+        {
+            get { return !string.IsNullOrWhiteSpace(Timeout) && !Timeout.StartsWith("["); }
+        }
+
+        public bool IsArbitraryWait
+        {
+            get { return !Choices.Any() && !string.IsNullOrWhiteSpace(Timeout); }
+        }
+    }
+}
