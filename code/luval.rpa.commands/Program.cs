a0db8532323294f1529a575cc181d2829e28fe4b
Oscar Marin <oscar@marin.cr>
2018-09-02 17:19:24 -0600

    important changes

--- code/luval.rpa.commands/Program.cs
+++ code/luval.rpa.commands/Program.cs
@@ -12,7 +12,7 @@ namespace luval.rpa.commands
     {
         static void Main(string[] args)
         {
-            var xml = File.ReadAllText(@"C:\Users\oscar.marin\Desktop\TMP\Tool-Release-v8.bprelease");
+            var xml = File.ReadAllText(@"C:\Users\oscar.marin\Desktop\TMP\Tool-Release-v9.bprelease");
             var release = new ReleaseExtractor(xml);
             release.Load();
         }
--- code/luval.rpa.common/Model/ObjectStage.cs
+++ code/luval.rpa.common/Model/ObjectStage.cs
@@ -9,14 +9,36 @@ namespace luval.rpa.common.Model
 {
     public class ObjectStage : Stage
     {
+
+        private XElement _processXml;
+
         public ObjectStage(XElement el) : base(el)
         {
             Actions = new List<ActionStage>();
             IdName = "id";
+            _processXml = GetElement("process");
         }
 
         public List<Stage> InitializeAction { get; set; }
         public List<ActionStage> Actions { get; set; }
         public ApplicationDefinition ApplicationDefinition { get; set; }
+
+        public string BpVersion
+        {
+            get { return GetAttributeValue(_processXml, "bpversion"); }
+            set { TrySetAttValue(_processXml, "bpversion", value); }
+        }
+
+        public string Description
+        {
+            get { return GetAttributeValue(_processXml, "narrative"); }
+            set { TrySetAttValue(_processXml, "narrative", value); }
+        }
+
+        public string RunMode
+        {
+            get { return GetAttributeValue(_processXml, "runmode"); }
+            set { TrySetAttValue(_processXml, "runmode", value); }
+        }
     }
 }
