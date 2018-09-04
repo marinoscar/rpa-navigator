8cab7a7d2288ffea3d512b859765e42994987913
Oscar Marin <oscar@marin.cr>
2018-09-04 08:23:33 -0600

    added configuration options

---
+++ code/luval.rpa.rules/Configuration/RuleConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace luval.rpa.rules.core.Configuration
+{
+    public class RuleConfiguration : ConfigurationElement
+    {
+        [ConfigurationProperty("assembly", DefaultValue = "", IsRequired = true)]
+        public string Assembly
+        {
+            get { return Convert.ToString(this["assembly"]); }
+            set { this["assembly"] = value; }
+        }
+    }
+}
---
+++ code/luval.rpa.rules/Configuration/RuleSectionConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace luval.rpa.rules.core.Configuration
+{
+    public class RuleAssembly : ConfigurationSection
+    {
+        [ConfigurationProperty("assembly", DefaultValue ="", IsRequired =true)]
+        public string Assembly
+        {
+            get { return Convert.ToString(this["assembly"]); }
+            set { this["assembly"] = value; }
+        }
+
+        [ConfigurationProperty("loadAllRulesInAssembly", DefaultValue = false, IsRequired = true)]
+        public bool LoadAllRulesInAssembly
+        {
+            get { return Convert.ToBoolean(this["loadAllRulesInAssembly"]); }
+            set { this["loadAllRulesInAssembly"] = value; }
+        }
+    }
+}
