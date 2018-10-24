be4863547df090104e8b055db012a35db9ef2ee0
Oscar Marin <oscar@marin.cr>
2018-10-23 19:16:35 -0600

    improved the name of the rule

--- code/luval.rpa.rules/ExceptionRecoveryOnMainPage.cs
+++ code/luval.rpa.rules/ExceptionRecoveryOnMainPage.cs
@@ -9,15 +9,15 @@ using luval.rpa.rules.core.Attributes;
 
 namespace luval.rpa.rules
 {
-    [Name("Exception handling on main page"),
-     Description("Checks that the main page of the process has exception handling")]
-    public class ExceptionHandlingOnMainPage : RuleBase, IRule
+    [Name("Exception recovery on main page"),
+     Description("Checks that the main page of the process has a way to recover from an exception")]
+    public class ExceptionRecoveryOnMainPage : RuleBase, IRule
     {
         public override IEnumerable<Result> Execute(Release release)
         {
-            var processes = release.Processes.Where(i => !i.MainPage.Any(e => e.Type == "Exception")).ToList();
+            var processes = release.Processes.Where(i => !i.MainPage.Any(e => e.Type == "Recover")).ToList();
             return processes.Select(i => FromPageBasedStage(i, ResultType.Error, "Process", 
-                string.Format("Process {0} does not have a exception hadling on the main page", i.Name), ""));
+                string.Format("Process {0} does not have a recover stage on the main page", i.Name), ""));
         }
     }
 }
