b03e3889c4739ed18953a31b2f2ef319cddb665f
Oscar Marin <oscar@marin.cr>
2018-10-26 11:36:59 -0600

    code clean up

--- code/luval.rpa.commands/Program.cs
+++ code/luval.rpa.commands/Program.cs
@@ -17,31 +17,7 @@ namespace luval.rpa.commands
     {
         static void Main(string[] args)
         {
-            var prof = @"profile.xml";
-            var ser = new XmlSerializer(typeof(RuleProfile));
-
-            //var profile = new RuleProfile();
-            //profile.Rules.Add(new Rule() { AssemblyFile = typeof(luval.rpa.rules.DataItemsNotInitialized).Assembly.FullName });
-            //profile.Exclusions = new List<Exclusion>(
-            //    new[] {
-            //        new Exclusion() { Name = "MS Excel VBO"},
-            //        new Exclusion() { Name = "Utility - Environment"},
-            //        new Exclusion() { Name = "Utility - General"},
-            //        new Exclusion() { Name = "Utility - Collection Manipulation"},
-            //    });
-            //var sw = new StringWriter();
-            //ser.Serialize(sw, profile);
-            //File.WriteAllText(prof, sw.ToString());
-
-            var newProfile = (RuleProfile)ser.Deserialize(File.OpenText(prof));
-            var xml = File.ReadAllText(@"C:\Users\oscar.marin\Desktop\TMP\NA_CS_OTC_0001_Order Entry_v3.bprelease");
-            var release = new ReleaseExtractor(xml);
-            release.Load();
-            var ruleEngine = new Runner();
-            var rules = ruleEngine.GetRulesFromProfile(newProfile);
-            var results = ruleEngine.RunRules(newProfile, release.Release, rules);
-            var report = new CodeReviewReportGenerator(newProfile, release.Release, results, rules);
-            report.ToCsv(@"report.csv");
+            
         }
     }
 }
