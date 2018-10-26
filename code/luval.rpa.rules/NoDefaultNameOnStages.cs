e88acffbf1ea16dca898564bc8dd90724edcc258
Oscar Marin <oscar@marin.cr>
2018-10-26 15:44:36 -0600

    added a new rule

---
+++ code/luval.rpa.rules/NoDefaultNameOnStages.cs
@@ -0,0 +1,34 @@
+﻿using luval.rpa.rules.core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using luval.rpa.common.Model;
+using luval.rpa.rules.core.Attributes;
+using System.Text.RegularExpressions;
+
+namespace luval.rpa.rules
+{
+    [Name("No Default Name On Stages"),
+     Description("Checks that there is a meaningful name for the stage")]
+    public class NoDefaultNameOnStages : RuleBase, IRule
+    {
+        const string _exp = "{0} *[1-9]*";
+        public override IEnumerable<Result> Execute(Release release)
+        {
+            var exclusions = GetExlusions().Split(",".ToArray());
+            var units = release.GetAnalysisUnits().Where(i =>
+                !exclusions.Contains(i.Stage.Type) &&
+                Regex.IsMatch(i.Stage.Name.ToLowerInvariant(), string.Format(_exp, i.Stage.Type.ToLowerInvariant()))).ToList();
+            var res = units.Select(i => FromStageAnalysis(i, ResultType.Warning, 
+                string.Format("Stage name {0} is not a proper name", i.Stage.Name), "")).ToList();
+            return res;
+        }
+
+        private string GetExlusions()
+        {
+            return GetSetting<string>("Exclusions", "Anchor,Start,End,Note,Recover,Resume");
+        }
+    }
+}
