a54f915fbc9b1fed8496432cee378e5e6f06dba8
Oscar Marin <oscar@marin.cr>
2018-10-26 19:23:09 -0600

    added a new reference and a new class

---
+++ code/luval.rpa.rules.core/JsonOuput.cs
@@ -0,0 +1,19 @@
+﻿using luval.rpa.common.Model;
+using luval.rpa.rules.core.Configuration;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace luval.rpa.rules.core
+{
+    public class JsonOuput
+    {
+        public JToken CreateReport(RuleProfile profile, IEnumerable<Result> results, Release release)
+        {
+            
+        }
+    }
+}
