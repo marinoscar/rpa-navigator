fb8f0ac628493ddaff29dc7f4925ade69b22fc64
Oscar Marin <oscar@marin.cr>
2018-10-26 18:30:55 -0600

    Fix the rule

--- code/luval.rpa.rules/AppModellerElementAttrCount.cs
+++ code/luval.rpa.rules/AppModellerElementAttrCount.cs
@@ -16,6 +16,7 @@ namespace luval.rpa.rules
         public override IEnumerable<Result> Execute(Release release)
         {
             var res = new List<Result>();
+            var helper = new StageHelper();
             foreach (var obj in release.Objects)
             {
                 foreach (var el in obj.ApplicationDefinition.Elements.Where(i => !string.IsNullOrWhiteSpace(i.Type) && i.Type.ToLowerInvariant().Contains("html")))
@@ -24,7 +25,7 @@ namespace luval.rpa.rules
                     var max = GetMaxCount();
                     if (atts.Count <= max)
                         continue;
-
+                    var uses = helper.ElementUses(el, obj);
                     res.Add(new Result()
                     {
                         Type = ResultType.Warning,
@@ -33,8 +34,8 @@ namespace luval.rpa.rules
                         Scope = "Object",
                         RuleName = Name,
                         RuleDescription = GetRuleDescription(),
-                        Message = string.Format("Too many attributes are used to identify the element {0}, a total of {1} are in used, and the max allowed is {2}. List attributes {3}"
-                        , el.Name, atts.Count, max, string.Join(", ", atts.Select(i => i.Name)))
+                        Message = string.Format("Too many attributes are used to identify the element {0}, a total of {1} are in used, and the max allowed is {2}. List attributes {3}. Element is used {4} times in stages {5}"
+                        , el.Name, atts.Count, max, string.Join(", ", atts.Select(i => i.Name)), uses.Count(), string.Join(",", uses.Select(i => i.Name)))
                     });
                 }
             }
