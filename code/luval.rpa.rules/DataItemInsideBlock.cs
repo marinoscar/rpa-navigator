6c60646a53116223fb4eff613111d25a98ebb143
Oscar Marin <oscar@marin.cr>
2018-11-05 19:44:58 -0600

    added helper class to enable the rule execution

--- code/luval.rpa.rules/DataItemInsideBlock.cs
+++ code/luval.rpa.rules/DataItemInsideBlock.cs
@@ -16,8 +16,44 @@ namespace luval.rpa.rules
     {
         public override IEnumerable<Result> Execute(Release release)
         {
-            var units = release.GetAnalysisUnits().Where(i => i.Stage.Type == "Data" || i.Stage.Type == "Block").ToList();
-            throw new NotImplementedException();
+            var res = new List<Result>();
+            ProcessItem(res, release.Processes, "Process");
+            ProcessItem(res, release.Objects, "Object");
+            return res;
+        }
+
+        private void ProcessItem(List<Result> res, IEnumerable<PageBasedStage> items, string type)
+        {
+            foreach (var item in items)
+            {
+                var pageBlock = new PageBlockGroup();
+                pageBlock.Load(FilterByPage(item.MainPage), item.Name, "Main", type);
+                LoadErrors(res, pageBlock);
+                foreach (var page in item.Pages)
+                {
+                    pageBlock.Load(page.Stages, item.Name, page.Name, type);
+                    LoadErrors(res, pageBlock);
+                }
+            }
+        }
+
+        private void LoadErrors(List<Result> results, PageBlockGroup pageBlock)
+        {
+            foreach(var stage in pageBlock.StagesOutsideOfBlock)
+            {
+                results.Add(new Result() {
+                    RuleName = Name, RuleDescription = GetRuleDescription(),
+                    Parent = pageBlock.Parent, Page = pageBlock.PageName,
+                    Scope = pageBlock.Type, Type = ResultType.Warning,
+                    Stage = stage.Name, StageType = stage.Type, StageId = stage.Id,
+                    Message = string.Format("Data Item {0} is outside of a block stage", stage.Name)
+                });
+            }
+        }
+
+        private IEnumerable<Stage> FilterByPage(IEnumerable<Stage> stages)
+        {
+            return stages.Where(i => i.Type == "Block" || i.Type == "Data");
         }
 
         
