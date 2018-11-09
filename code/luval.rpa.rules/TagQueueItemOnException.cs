2079e184f90d834e0ab21298819d00262131d1de
Oscar Marin <oscar@marin.cr>
2018-11-09 12:22:16 -0600

    added a new class

---
+++ code/luval.rpa.rules/TagQueueItemOnException.cs
@@ -0,0 +1,23 @@
+﻿using luval.rpa.rules.core;
+using luval.rpa.rules.core.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using luval.rpa.common.Model;
+
+namespace luval.rpa.rules
+{
+    [Name("Data Items Are Inside Block Stage"),
+     Description("Checks that all data items are inside a block stage")]
+    public class TagQueueItemOnException : RuleBase, IRule
+    {
+        public override IEnumerable<Result> Execute(Release release)
+        {
+            var res = new List<Result>();
+
+            return res;
+        }
+    }
+}
