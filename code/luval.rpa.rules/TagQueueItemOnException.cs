b90c87a793a208feda9a304d3142571171c57210
Oscar Marin <oscar@marin.cr>
2018-11-09 12:55:51 -0600

    added basic logic for the rule

--- code/luval.rpa.rules/TagQueueItemOnException.cs
+++ code/luval.rpa.rules/TagQueueItemOnException.cs
@@ -15,9 +15,35 @@ namespace luval.rpa.rules
     {
         public override IEnumerable<Result> Execute(Release release)
         {
+            var helper = new StageHelper();
             var res = new List<Result>();
-
+            var units = release.GetAnalysisUnits(i => i.Type == "Action" || i.Type == "Anchor");
+            var stages = units.Select(i => i.Stage).ToList();
+            var mark = GetQueueStages(stages.Where(i => i.Type == "Action"), "Mark Exception").ToList();
+            var tag = GetQueueStages(stages.Where(i => i.Type == "Action"), "Tag Item").ToList();
+            foreach (var ac in mark)
+            {
+                //no tag for the mark exception
+                if (!tag.Any(i => helper.GetNextStage(ac.Id, stages).Id == i.Id))
+                    res.Add(FromStageAnalysis(units.Single(i => i.Stage.Id == ac.Id),
+                        ResultType.Error, string.Format("Mark exception requires that item has the exception labeled"),
+                        ""
+                        ));
+            }            
             return res;
+        }
+
+        private IEnumerable<ActionStage> GetQueueStages(IEnumerable<Stage> stages)
+        {
+            //!string.IsNullOrWhiteSpace(i.Resource.Action) && i.Resource.Action == "Mark Exception"
+            return stages.Select(i => (ActionStage)i)
+                .Where(i => i.Resource != null && !string.IsNullOrWhiteSpace(i.Resource.Object) &&
+                i.Resource.Object == "Blueprism.Automate.clsWorkQueuesActions").ToList();
+        }
+
+        private IEnumerable<ActionStage> GetQueueStages(IEnumerable<Stage> stages, string actionFilter)
+        {
+            return GetQueueStages(stages).Where(i => !string.IsNullOrWhiteSpace(i.Resource.Action) && i.Resource.Action == actionFilter).ToList();
         }
     }
 }
